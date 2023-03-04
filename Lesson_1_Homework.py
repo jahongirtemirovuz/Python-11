e07ef8b5c8b1b210471d0b5f195f88a66d21e1b5
jahongirtemirovuz <125241434+jahongirtemirovuz@users.noreply.github.com>
2023-03-04 10:44:39 +0500

    Add files via upload

---
+++ Lesson_1_Homework.py
@@ -0,0 +1,57 @@
+                  # Topshiriq-1-Foydalanuvchi ismnini... terminalga chiqarish
+first_name = input("Ismingizni kiriting: ")
+last_name = input("Familiyangizni kiriting: ")
+age = int(input("Yoshingizni kiriting: "))
+email = input("E-pochtangizni kiriting: ")
+password = int(input("Parolni kiriting: "))
+full_info = f"Ismingiz: {first_name}. Familiyangiz: {last_name}. Yoshingiz: {age}. E-pochtangiz: {email}. Parolingiz: {password}."
+print(full_info)
+
+                          # Topshiriq-2-masala yechish
+# # 1) O`lchami x bo`lgan kubik berilgan uni hajmini toping.
+a = int(input('Kubning uzunligini kiriting: '))
+z= a**3
+print(z)
+
+# # 2) a,b va c tamonli uch burchak berilgan. Uchburchakning perimetri topilsin.
+a = int(input("Birinchi tomon: "))
+b = int(input("Ikkinchi tomon: "))
+c = int(input("Uchinchi tomon: "))
+z = a + b + c
+print(z)
+
+# # 3) Radiuslari r1, r2, r3 bo’lgan 3 to doira radiuslari berilgan. Doiralarni yuzlarinihisoblang.
+r1 = int(input("Birinchi radius: "))
+pi = 3.14
+z = 2*pi*r1
+print(z)
+
+# # 4) Sinf xonasining eni 10 metr, bo‘yi 12 metr. Xonaning yuzini topish dasturini tuzing. 
+# Eni va boyi qiymatlari foydalanuvchi tomonidan kiritilsin
+a=int(input("Eni: "))
+b=int(input("Bo'yi: "))
+s = a*b
+print(s)
+
+
+#                                         # Boolean
+
+# Birinchi
+day = 15
+month = 3
+day == 15 and month == 3
+
+# # Ikkinchi
+5 + 6 > 4
+
+# Uchinchi
+a=10
+b=a+4
+a+3>=b-5
+
+                                            #\n  methodi
+print("Good morning .\nWelcome")
+
+
+
+
