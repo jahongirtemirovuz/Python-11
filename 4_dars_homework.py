535c89fdfdbcf666ec7fb6aa4ad0b6985d5ca5bd
jahongirtemirovuz <125241434+jahongirtemirovuz@users.noreply.github.com>
2023-03-09 10:21:11 +0500

    Add files via upload

---
+++ 4_dars_homework.py
@@ -0,0 +1,51 @@
+# # # # # #######################--Kompyuter o'ylagan sonni topish--#################################
+# import random
+# number = random.randint(1, 40)
+# for i in range(1, 40)
+# guess = 0
+# count = 0
+# while guess != number:
+#     guess = int(input("Enter a number(between 1 and 40): "))
+# count += 1
+# if guess < number:
+#     print("Too low, try again")
+# elif guess > number:
+#     print("Too high, try again")
+# else:
+#     print("Congratulations!")
+
+
+# # # # # ####################################--Katta yoki kichikligini aniqlovchi dastur--##################
+# a = int(input("1-chi sonni kiriting: "))
+# b = int(input("2-chi sonni kiriting: "))
+
+# # a, b = map(float, input("Enter two numbers: ").split())
+# if a > b:
+#     print("The largest number is:",a)
+# else:
+#     print("The largest number is:",b)
+
+
+# # # # #######################################--Teskari so'z o'yini--#############################################
+# word = input("So'z kiriting: ")
+# if word == word[::-1]:
+#     print("To'g'ri")
+# else:
+#     print("Noto'g'ri")
+
+
+# # ########################################--To'g'ri registratsiya dasturi--##################################
+# password = input("Parolni kiriting: ")
+# if len(password) < 8 or len(password) > 20:
+#     print("Parol 8 tadan 20 tagacha simvollardan iborat bo'lishi kerak!")
+# else:
+#     print("Parol to'g'ri")
+
+
+#########################--Bo'linishi yoki bo'linmasligini tekshiruvchi dastur--#############
+# a = int(input("1-chi raqamni kiriting: "))
+# b = int(input("2-chi raqamni kiriting: "))
+
+
+
+
